69b34b95e65c9a989ca965fc5c04b8354d7bc3d6
Michael Cooper <mythmon@gmail.com>
2025-04-08 10:33:27 -0700

    remove todos replaced by issues

--- pkgs/ganzu/src/index.ts
+++ pkgs/ganzu/src/index.ts
@@ -8,18 +8,3 @@ export type { LoadConfigReturn } from "./loader.ts";
 export { loadConfig } from "./loader.ts";
 export type { ConfigDefinition } from "./definition.ts";
 export { g } from "./definition.ts";
-
-// TODO
-// - booleans
-// - enums
-// - arbitrary zod?
-// - document environment variable naming
-// - array values
-// - map values
-// - struct values
-// - immutable field definitions
-// - custom error messages
-// - TOML support
-// - KDL support?
-// - 1 Password support
-// - .env support
